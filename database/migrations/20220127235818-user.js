b06259dc079e918046ceda8ef723f3bde3e86a10
Gabriel Guimaraes <gabrielgs1408@gmail.com>
2022-02-01 15:01:44 -0300

    initial commit

---
+++ database/migrations/20220127235818-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+     await queryInterface.createTable('users', 
+      { 
+        id: { 
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        }, 
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        }
+      }, 
+      
+     );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('users');
+  }
+};
